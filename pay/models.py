b096176fe0956f8001d295c757a68d5c86398924
brytfrost <brytfrost@gmail.com>
2024-01-27 22:18:24 +0000

    Changes to be committed: 	modified:   pay/models.py 	modified:   paystack_backend_pay/settings.py 	modified:   paystack_backend_pay/urls.py

--- pay/models.py
+++ pay/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Payment(models.Model):
+    """Model definition for Payment."""
+
+    email=models.EmailField()
+    ref=models.CharField(max_length=50)
+    amount=models.PositiveIntegerField()
+    date_created=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Payment."""
+
+        verbose_name = 'Payment'
+        verbose_name_plural = 'Payments'
+
+    def __str__(self):
+        """Unicode representation of Payments."""
+        return '{}'.format(self.amount ) # TODO
